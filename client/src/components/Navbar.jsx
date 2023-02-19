256cee1e4afb7a7a7d9f19104bbf5266e574487d
bodwood <bodiewood@hotmail.com>
2023-02-19 11:23:43 -0800

    create function so navbar links are modular

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,14 +7,34 @@ import {
   Icon,
   Text,
   useDisclosure,
-  Button,
-  Stack,
+  // Button,
+  // Stack,
   useColorMode,
   useColorModeValue,
 } from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
 import { Link as ReactLink } from 'react-router-dom';
 import { FaHelicopter } from 'react-icons/fa';
+
+const linkArray = [
+  { linkName: 'Products', path: '/products' },
+  { linkName: 'ShoppingCart', path: '/cart' },
+];
+
+const NavLink = ({ path, children }) => {
+  return (
+    <Link
+      as={ReactLink}
+      to={path}
+      px={2}
+      py={2}
+      rounded='md'
+      _hover={{ textDecoration: 'none', bg: useColorModeValue('gray.200', 'gray.700') }}
+    >
+      {children}
+    </Link>
+  );
+};
 
 const Navbar = () => {
   const { isOpen, onClose, onOpen } = useDisclosure();
@@ -32,11 +52,32 @@ const Navbar = () => {
         <HStack>
           <Link as={ReactLink} to='/'>
             <Flex alignItems='center'>
-              <Icon as={FaHelicopter} h={59} w={59} color='black'/>
-              <Text fontWeight='extrabold' fontSize='20px'>Gear Up</Text>
+              <Icon as={FaHelicopter} h={59} w={59} color='black' />
+              <Text fontWeight='extrabold' fontSize='20px'>
+                Gear Up
+              </Text>
             </Flex>
           </Link>
+          <HStack>
+            {linkArray.map((e) => {
+              const { linkName, path } = e;
+              return (
+                <NavLink key={linkName} path={path}>
+                  {linkName}
+                </NavLink>
+              );
+            })}
+          </HStack>
         </HStack>
+        <Flex alignItems='center'>
+          <NavLink>
+            <Icon
+              as={colorMode === 'light' ? MoonIcon : SunIcon}
+              alignSelf='center'
+              onClick={() => toggleColorMode()}
+            />
+          </NavLink>
+        </Flex>
       </Flex>
     </Box>
   );
