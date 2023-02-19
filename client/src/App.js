402817fdefbcf24f617fdd4b3aad08345e7c9cad
bodwood <bodiewood@hotmail.com>
2023-02-19 10:25:41 -0800

    create basic navbar with helicopter icon and application name. i.e. brand name

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
-import { ChakeraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
 function App() {
   return (
-  <ChakeraProvider>
+  <ChakraProvider>
     <Router>
       <Navbar />
       <main></main>
     </Router>
 
-  </ChakeraProvider>
+  </ChakraProvider>
   );
 }
 
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import {
+  Box,
+  Flex,
+  HStack,
+  Link,
+  IconButton,
+  Icon,
+  Text,
+  useDisclosure,
+  Button,
+  Stack,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { Link as ReactLink } from 'react-router-dom';
+import { FaHelicopter } from 'react-icons/fa';
+
+const Navbar = () => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+      <Flex h={16} alignItems='center' justifyContent={'space-between'}>
+        <IconButton
+          size='md'
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          display={{ md: 'none' }}
+          onClick={isOpen ? onClose : onOpen}
+        />
+
+        <HStack>
+          <Link as={ReactLink} to='/'>
+            <Flex alignItems='center'>
+              <Icon as={FaHelicopter} h={59} w={59} color='black'/>
+              <Text fontWeight='extrabold' fontSize='20px'>Gear Up</Text>
+            </Flex>
+          </Link>
+        </HStack>
+      </Flex>
+    </Box>
+  );
+};
+export default Navbar;
