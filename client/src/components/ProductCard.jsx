66f72a1323eba42a804c900d9e03fe478e76a45c
bodwood <bodiewood@hotmail.com>
2023-02-22 18:23:41 -0800

    update product list display to show title, price, and have a hover effect that changes pointer to ghost

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -35,7 +35,27 @@ const ProductCard = ({ product }) => {
       {product.isNew && <Circle size='10px' position='absolute' top={2} right={2} bg='green.300' />}
       {product.stock <= 0 && <Circle size='10px' position='absolute' top={2} right={2} bg='red.300' />}
       <Image src={product.image} alt={product.name} roundedTop='lg' />
-      <Box></Box>
+      <Box>
+        {product.stock <= 0 && <Badge>Sold Out</Badge>}
+        {product.isNew && <Badge>New</Badge>}
+      </Box>
+      <Flex>
+        <Link as={ReactLink} to={`/product${product._id}`}>
+        <Box>{product.name}
+          </Box>
+        </Link>
+      </Flex>
+      <Flex>
+        <Box>
+          <Box>$</Box>
+          {product.price.toFixed(2)}
+        </Box>
+        <Tooltip label='Add to cart' bg='white' placement='top' color='gray.800' fontSize='1.2em'>
+          <Button variant='ghost' display='flex' disabled={product.stock <= 0}>
+            <Icon as={FiShoppingCart} h={7} w={7} alignSelf='center/'></Icon>
+          </Button>
+        </Tooltip>
+      </Flex>
     </Stack>
   );
 };
