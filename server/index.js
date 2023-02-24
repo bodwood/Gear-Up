e76ccbc0e1b9a72f443537e6a491e5d1e6181a16
bodwood <bodiewood@hotmail.com>
2023-02-24 14:15:29 -0800

    add api route

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
+//sets up Express.js server that listens on specified port
 import dotenv from 'dotenv'
-
 import connectToDatabase from './database.js';
 import express from 'express'
 
-dotenv.config()
-connectToDatabase();
-const app = express();
+//Routes
+import productRoutes from './routes/productRoutes.js'
 
-app.use(express.json());
+dotenv.config() //loads environment variables from .env
+connectToDatabase(); 
+const app = express(); //creates new express application
+
+app.use(express.json()); //sets up middleware to parse incoming json data
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => {
+app.use('/api/products', productRoutes);
+
+app.listen(port, () => { //starts server
  console.log(`Server runs on port ${port}.`);
 })
