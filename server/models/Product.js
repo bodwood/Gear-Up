9c1ecd602e96febbec3cd75dda05945c410dd52f
bodwood <bodiewood@hotmail.com>
2023-02-24 12:05:41 -0800

    add schema for products

--- server/models/Product.js
+++ server/models/Product.js
@@ -11,3 +11,57 @@ const reviewSchema = new Schema(
   },
   { timestamps: true }
 );
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numberOfReviews: {
+      type: Number,
+      required: true,
+    },
+    brand: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    productIsNew: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
