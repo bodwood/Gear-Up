bb0dcb20564aa704e6cc4f65e00b27cf7a661adf
bodwood <bodiewood@hotmail.com>
2023-02-24 11:40:44 -0800

    remove console log

--- server/database.js
+++ server/database.js
@@ -3,8 +3,6 @@
 import mongoose from 'mongoose';
 
 const connectToDatabase = async () => {
-  console.log(process.env.MONGO_URI);
-
   try {
     mongoose.set('strictQuery', false);
     const connect = await mongoose.connect(process.env.MONGO_URI, {
