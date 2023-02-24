cb51f37170e79fea18653d2997fa013eb4a49e90
bodwood <bodiewood@hotmail.com>
2023-02-24 11:40:11 -0800

    add port listener to be either local host or port 5000

--- server/index.js
+++ server/index.js
@@ -8,3 +8,9 @@ connectToDatabase();
 const app = express();
 
 app.use(express.json());
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+ console.log(`Server runs on port ${port}.`);
+})
