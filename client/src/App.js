e1f62c7b414e1b7e0f73583d11473ab2a220aa0f
bodwood <bodiewood@hotmail.com>
2023-02-26 13:50:36 -0800

    add route for path to cart screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import CartScreen from './screens/CartScreen';
 import ProductsScreen from './screens/ProductsScreen';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <main>
           <Routes>
             <Route path='/products' element={<ProductsScreen />}></Route>
+            <Route path='/cart' element={<CartScreen />}></Route>
           </Routes>
         </main>
       </Router>
