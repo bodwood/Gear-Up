ad39158f79eff1a4278cb6e49a1631af3ff2f0d2
bodwood <bodiewood@hotmail.com>
2023-02-26 17:54:34 -0800

    add function to return product to user when order summary page is requested

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,12 +1,23 @@
-import express from 'express'
-import Product from '../models/Product.js'
+import express from 'express';
+import Product from '../models/Product.js';
 const productRoutes = express.Router();
 
 const getProducts = async (req, res) => {
- const products = await Product.find({});
- res.json(products);
-}
+  const products = await Product.find({});
+  res.json(products);
+};
+
+const getProduct = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product not found.');
+  }
+};
 
 productRoutes.route('/').get(getProducts);
+productRoutes.route('/:id').get(getProduct);
 
 export default productRoutes;
