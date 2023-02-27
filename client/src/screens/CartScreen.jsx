c2e5cabb9b1a7e598ca6284dec25f40eaefa82c5
bodwood <bodiewood@hotmail.com>
2023-02-26 17:52:57 -0800

    update Stack to include CartOrderSummary to cart screen

--- client/src/screens/CartScreen.jsx
+++ client/src/screens/CartScreen.jsx
@@ -15,6 +15,8 @@ import {
 } from '@chakra-ui/react';
 import { Link as ReactLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import CartItem from '../components/CartItem'
+import CartOrderSummary from '../components/CartOrderSummary';
 
 const CartScreen = () => {
   const cartInfo = useSelector((state) => state.cart);
@@ -60,10 +62,17 @@ const CartScreen = () => {
               <Heading fontSize='2xl' fontWeight='extrabold'>
                 Shopping Cart {getHeadingContent()}
               </Heading>
-              <Stack spacing='6'>{/* CartItem */}</Stack>
+              <Stack spacing='6'>
+                {cart.map((cartItem) => {
+                  return (
+                    <CartItem key={cartItem.id} cartItem={cartItem} />
+                  )
+                })}
+                
+                </Stack>
             </Stack>
             <Flex direction='column' align='center' flex='1'>
-              {/* CartOrderSummary */}
+              <CartOrderSummary />
 
               <HStack mt='6' fontWeight='semibold'>
                 <p>or</p>
