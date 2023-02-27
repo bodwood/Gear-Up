f6fb262153116e8883542ddd8de5f1ea3946cac9
bodwood <bodiewood@hotmail.com>
2023-02-26 17:39:56 -0800

    Create styling for order summary. Displays each item that a user has in their cart.

---
+++ client/src/components/CartOrderSummary.jsx
@@ -0,0 +1,59 @@
+import { Button, Flex, Heading, useColorModeValue as mode, Stack, Text, Badge } from '@chakra-ui/react';
+import { useState } from 'react';
+import { FaArrowRight } from 'react-icons';
+import { useSelector } from 'react-redux';
+import { Link as ReactLink, useNavigate } from 'react-router-dom';
+
+const CartOrderSummary = () => {
+  const [buttonLoading, setButtonLoading] = useState();
+  const standardShipping = Number(4.99).toFixed(2);
+  const cartItems = useSelector((state) => state.cart);
+  const { subtotal } = cartItems;
+  const navigate = useNavigate;
+
+  const checkoutHandler = () => {
+   
+  }
+
+  return (
+    <Stack spacing='8' borderWidth='1px' rounded='lg' padding='8' w='full'>
+      <Heading size='md'>Order Summary</Heading>
+      <Stack spacing='6'>
+        <Flex justify='space-between'>
+          <Text fontWeight='medium' color={mode('gray.600', 'gray.400')}>
+            Subtotal
+          </Text>
+          <Text fontWeight='medium'>{subtotal}</Text>
+        </Flex>
+        <Flex justify='space-between'>
+          <Text fontWeight='medium' color={mode('gray.600', 'gray.400')}>
+            Shipping
+          </Text>
+          <Text fontWeight='medium'>
+            {subtotal <= 1000 ? (
+              standardShipping
+            ) : (
+              <Badge rounded='full' px='2' fontSize='0.8em' colorScheme='green'>
+                Free
+              </Badge>
+            )}
+          </Text>
+        </Flex>
+        <Flex fontSize='lg' fontWeight='semibold'>
+          {subtotal <= 1000 ? Number(subtotal) + Number(standardShipping) : subtotal}
+        </Flex>
+      </Stack>
+      <Button
+        as={ReactLink}
+        to='/checkout'
+        colorScheme='orange'
+        size='lg'
+        fontSize='md'
+        rightIcon={<FaArrowRight />}
+        isLoading={buttonLoading}
+        onClick={() => checkoutHandler()}
+      >Checkout</Button>
+    </Stack>
+  );
+};
+export default CartOrderSummary;
