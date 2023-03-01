d494eabc62fc05905494ba05b403ae659428ccf9
bodwood <bodiewood@hotmail.com>
2023-02-28 19:22:01 -0800

    add dispatch function to update state of cart and remove all items that do not match the id of the current state

--- client/src/redux/slices/cart.js
+++ client/src/redux/slices/cart.js
@@ -44,10 +44,17 @@ export const cartSlice = createSlice({
       state.error = payload;
       state.loading = false;
     },
+    cartItemRemoval: (state, {payload}) => {
+      state.cart = [...state.cart].filter((item) => item.id !== payload);
+      updateLocalStorage(state.cart);
+      state.subtotal = calculateSubtotal(state.cart)
+      state.loading = false;
+      state.error = null;
+    }
   },
 });
 
-export const { setLoading, setError, setProducts, cartItemAdd } = cartSlice.actions;
+export const { setLoading, setError, setProducts, cartItemAdd, cartItemRemoval } = cartSlice.actions;
 export default cartSlice.reducer;
 
 export const cartSelector = (state) => state.cart;
