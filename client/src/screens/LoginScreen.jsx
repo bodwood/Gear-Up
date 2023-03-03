751bc98497327c8800c16c11cd0b337385e103b7
bodwood <bodiewood@hotmail.com>
2023-03-03 14:41:46 -0800

    add chakra boxes and formatting for login and password fields, also alert box was added for unsuccessful logins

--- client/src/screens/LoginScreen.jsx
+++ client/src/screens/LoginScreen.jsx
@@ -24,6 +24,9 @@ import { useNavigate, Link as ReactLink, useLocation } from 'react-router-dom';
 
 //TODO: redefine password min length
 const LoginScreen = () => {
+  const headingBR = useBreakPointValue({ base: 'xs', md: 'sm' });
+  const boxBR = useBreakPointValue({ base: 'transparent', md: 'bg-surface' });
+
   return (
     <Formik
       initialValues={{ email: '', password: '' }}
@@ -37,9 +40,50 @@ const LoginScreen = () => {
         dispatch(login(values.email, values.password));
       }}
     >
-      {(formik) => <Container maxW='lg' py={{ base: '12', md: '24' }} px={{ base: '0', md: '8' }} minH='4xl'>
-       
-       </Container>}
+      {(formik) => (
+        <Container maxW='lg' py={{ base: '12', md: '24' }} px={{ base: '0', md: '8' }} minH='4xl'>
+          <Stack spacing='8'>
+            <Stack spacing='6'>
+              <Stack spacing={{ base: '2', md: '3' }} textAlign='center'>
+                <Heading size={headingBR}>Log in to your account</Heading>
+                <HStack spacing='1' justify='center'>
+                  <Text color='muted'>Don't have an account ?</Text>
+                  <Button as={ReactLink} to='/registration' variant='link' colorScheme='orange'>
+                    Sign up
+                  </Button>
+                </HStack>
+              </Stack>
+            </Stack>
+            <Box
+              py={{ base: '0', md: '8' }}
+              px={{ base: '4', md: '10' }}
+              bg={{ boxBR }}
+              boxShadow={{ base: 'none', md: 'xl' }}
+            >
+              <Stack spacing='6' as='form' onSubmit={formik.handleSubmit}>
+                {error && (
+                  <Alert
+                    status='error'
+                    flexDirection='column'
+                    alignItems='center'
+                    justifyContent='center'
+                    textAlign='center'
+                  >
+                    <AlertIcon />
+                    <AlertTitle>Oops!</AlertTitle>
+                    <AlertDescription>{error}</AlertDescription>
+                  </Alert>
+                )}
+                <Stack spacing='5'>
+                 <FormControl>
+                  
+                 </FormControl>
+                </Stack>
+              </Stack>
+            </Box>
+          </Stack>
+        </Container>
+      )}
     </Formik>
   );
 };
