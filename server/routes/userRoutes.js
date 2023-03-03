d34a8d5dd545ed39066d1e7518463ebd7497d4f7
bodwood <bodiewood@hotmail.com>
2023-03-03 11:10:28 -0800

    create function that creates json web token for user sign-in

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import User from '../models/User';
+import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
+
+const router = express.Router();
+
+//TODO: redefine expiresIn
+//tokens usually expire in an hour
+const genToken = (id) => {
+  return jwt.sign({ id }, process.env.TOKEN_SECRET, { expiresIn: '60d' });
+};
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPasswords(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: genToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
