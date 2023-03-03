b9c22b95ce3ba899746090eb5277fd8b9b29ebe1
bodwood <bodiewood@hotmail.com>
2023-03-03 10:23:42 -0800

    update color scheme of landing page headers

--- client/src/screens/LandingScreen.jsx
+++ client/src/screens/LandingScreen.jsx
@@ -1,31 +1,53 @@
-import { Box, Flex, Heading, HStack, Icon, Image, Link, Skeleton, Stack, useColorModeValue } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+  Image,
+  Link,
+  Skeleton,
+  Stack,
+  useColorModeValue,
+  Text,
+} from '@chakra-ui/react';
 import { FaArrowRight } from 'react-icons/fa';
+import { Link as ReactLink } from 'react-router-dom';
+import { FaHelicopter } from 'react-icons/fa';
 
 const LandingScreen = () => (
-  <Box maxW='7xl' mx='auto' px={{ base: '0', lg: '12' }} py={{ base: '0', lg: '12' }}>
+  <Box maxW='8xl' mx='auto' px={{ base: '0', lg: '12' }} py={{ base: '0', lg: '12' }} minH='6xl'>
     <Stack direction={{ base: 'column-reverse', lg: 'row' }} spacing={{ base: '0', lg: '20' }}>
       <Box
         width={{ lg: 'sm' }}
         transform={{ base: 'translateY(-50%)', lg: 'none' }}
-        bg={{ base: useColorModeValue('red.50', 'gray.700'), lg: 'transparent' }}
+        bg={{ base: useColorModeValue('orange.50', 'gray.700'), lg: 'transparent' }}
         mx={{ base: '6', md: '8', lg: '0' }}
         px={{ base: '6', md: '8', lg: '0' }}
         py={{ base: '6', md: '8', lg: '12' }}
       >
         <Stack spacing={{ base: '8', lg: '10' }}>
           <Stack spacing={{ base: '2', lg: '4' }}>
-            <Heading size='xl' color={useColorModeValue('red.500', 'red.300')}>
-              Misguided
-            </Heading>
+            <Flex alignItem='center'>
+            </Flex>
+            <Text fontSize='4xl' fontWeight='bold' color={useColorModeValue('orange.500', 'orange.300')}>
+              Gear Up
+            </Text>
             <Heading size='xl' fontWeight='normal'>
-              Refresh your wardrobe
+              Refresh your kicks
             </Heading>
           </Stack>
           <HStack spacing='3'>
-            <Link color={useColorModeValue('red.500', 'red.300')} fontWeight='bold' fontSize='lg'>
+            <Link
+              as={ReactLink}
+              to='/products'
+              color={useColorModeValue('orange.500', 'orange.300')}
+              fontWeight='bold'
+              fontSize='lg'
+            >
               Discover now
             </Link>
-            <Icon color={useColorModeValue('red.500', 'red.300')} as={FaArrowRight} />
+            <Icon color={useColorModeValue('orange.500', 'orange.300')} as={FaArrowRight} />
           </HStack>
         </Stack>
       </Box>
