2fee0b6851b8034f817c4030c8491eb780cfe42e
bodwood <bodiewood@hotmail.com>
2023-03-03 14:10:04 -0800

    add Formik validation for email and password validation

--- client/src/screens/LoginScreen.jsx
+++ client/src/screens/LoginScreen.jsx
@@ -1,30 +1,46 @@
 import {
- Box,
- Button,
- Checkbox,
- Container,
- FormControl,
- Heading,
- HStack,
- Stack,
- Text,
- useBreakPointValue,
- useColorModeValue,
- Alert,
- AlertIcon,
- AlertTitle,
- AlertDescription,
- useToast,
-} from '@chakra-ui/react'
-import { useState, useEffect } from 'react'
-import { Formik } from 'formik'
-import * as Yup from 'yup'
-import { useSelector, useDispatch } from 'react-redux'
-import { useNavigate, Link as ReactLink, useLocation} from 'react-router-dom'
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormControl,
+  Heading,
+  HStack,
+  Stack,
+  Text,
+  useBreakPointValue,
+  useColorModeValue,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  useToast,
+} from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate, Link as ReactLink, useLocation } from 'react-router-dom';
 
+//TODO: redefine password min length
 const LoginScreen = () => {
   return (
-    <div>LoginScreen</div>
-  )
-}
-export default LoginScreen
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validationSchema={Yup.object({
+        email: Yup.string().email('Invalid email.').required('An email address is required.'),
+        password: Yup.string()
+          .min(1, 'Password is too short - must contain at least 1 character')
+          .required('Password is required'),
+      })}
+      onSubmit={(values) => {
+        dispatch(login(values.email, values.password));
+      }}
+    >
+      {(formik) => <Container maxW='lg' py={{ base: '12', md: '24' }} px={{ base: '0', md: '8' }} minH='4xl'>
+       
+       </Container>}
+    </Formik>
+  );
+};
+export default LoginScreen;
