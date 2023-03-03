1c943172970cefbdb6e669cb32cde95c7adb4d95
bodwood <bodiewood@hotmail.com>
2023-03-03 15:48:18 -0800

    add login function to grab user info from database

---
+++ client/src/redux/actions/userActions.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export const login = (email, password) => async (dispatch) => {
+  dispatch(setLoading(true));
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const { data } = await axios.post('/api/users/login', { email, password }, config);
+    dispatch(userLogin(data));
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch(
+      setError(
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+          ? error.message
+          : 'An unexpected error has occurred. Please try again later.'
+      )
+    );
+  }
+};
