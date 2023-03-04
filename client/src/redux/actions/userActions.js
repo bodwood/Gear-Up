ff145653c24c80dce7450e7dd6990aeef09f941c
bodwood <bodiewood@hotmail.com>
2023-03-04 14:55:27 -0800

    add function to pass new user information to database

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -28,4 +28,28 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo');
   dispatch(userLogout());
-}
+};
+
+export const register = (name, email, password) => async (dispatch) => {
+  dispatch(setLoading(true));
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const { data } = await axios.post('/api/users/register', { name, email, password }, config);
+    dispatch(userLogin(data));
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch(
+      setError(
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+          ? error.message
+          : 'An unexpected error has occurred. Please try again later.'
+      )
+    );
+  }
+};
