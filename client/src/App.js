b52f2eeabfc4d65b1788266fd84ec2c56136da63
bodwood <bodiewood@hotmail.com>
2023-03-04 13:23:01 -0800

    update Login Screen route tag to remove closing tag

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
             <Route path='/products' element={<ProductsScreen />}></Route>
             <Route path='/product/:id' element={<ProductScreen />}></Route>
             <Route path='/cart' element={<CartScreen />}></Route>
-            <Route path='/login' element={<LoginScreen />}></Route>
+            <Route path='/login' element={<LoginScreen />} />
           </Routes>
         </main>
         <Footer />
