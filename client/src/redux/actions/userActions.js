cb713eda2e62fbf53b08d55b2050af178b813631
bodwood <bodiewood@hotmail.com>
2023-03-04 13:58:23 -0800

    import logout management from user slice, remove userInfo from local stoage, export logout function

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { setLoading, setError, userLogin } from '../slices/user';
+import { setLoading, setError, userLogin, userLogout } from '../slices/user';
 
 export const login = (email, password) => async (dispatch) => {
   dispatch(setLoading(true));
@@ -24,3 +24,8 @@ export const login = (email, password) => async (dispatch) => {
     );
   }
 };
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo');
+  dispatch(userLogout());
+}
