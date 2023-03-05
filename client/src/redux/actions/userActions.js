666b77da10c2e764629c00b5d78100ae7e2388ea
bodwood <bodiewood@hotmail.com>
2023-03-04 19:46:55 -0800

    add function that updates user profile information and stores such information in database

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { setLoading, setError, userLogin, userLogout } from '../slices/user';
+import { setLoading, setError, userLogin, userLogout, updateUserProfile, resetUpdate } from '../slices/user';
 
 export const login = (email, password) => async (dispatch) => {
   dispatch(setLoading(true));
@@ -53,3 +53,36 @@ export const register = (name, email, password) => async (dispatch) => {
     );
   }
 };
+
+export const updateProfile = (id, name, email, password) => async (dispatch, getState) => {
+  const {
+    user: { userInfo },
+  } = getState();
+
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const { data } = await axios.put(`/api/users/profile${id}`, { _id: id, name, email, password }, config);
+    localStorage.setItem('userInfo', JSON.stringify(data));
+    dispatch(updateUserProfile(data));
+  } catch (error) {
+    dispatch(
+      setError(
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+          ? error.message
+          : 'An unexpected error has occurred. Please try again later.'
+      )
+    );
+  }
+};
+
+export const resetUpdateSuccess = () => async(dispatch) => {
+  dispatch(resetUpdate());
+}
