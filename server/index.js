284f226b8bcc1f5bc4e1474604fa2a8d072b8c5d
bodwood <bodiewood@hotmail.com>
2023-03-05 20:11:15 -0800

    add new order route to app connection wtih express

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import express from 'express'
 //Routes
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 
 dotenv.config() //loads environment variables from .env
 connectToDatabase(); 
@@ -18,6 +19,7 @@ const port = process.env.PORT || 5000
 //exposes routes to our URLs below
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes);
 
 app.listen(port, () => { //starts server
  console.log(`Server runs on port ${port}.`);
