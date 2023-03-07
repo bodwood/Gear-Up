29c173f181377c7db4bce49fad88a240c1e21bb9
bodwood <bodiewood@hotmail.com>
2023-03-06 18:44:27 -0800

    update minimum value to input fields and added function that disables paypal button when input fields are not filled

--- client/src/components/ShippingInformation.jsx
+++ client/src/components/ShippingInformation.jsx
@@ -9,19 +9,39 @@ import { setShippingAddress, setShippingAddressError } from '../redux/actions/or
 
 const ShippingInformation = () => {
   const dispatch = useDispatch();
+  const [formStateChanged, setFormStateChanged] = useState(false);
+
+  const setErrorState = (input, data) => {
+   if(!input) {
+    dispatch(setShippingAddress(data))
+   }
+   if((!formStateChanged && !input) || (formStateChanged && input)) {
+    return;
+   } else {
+    setFormStateChanged(input)
+    dispatch(setShippingAddressError(input))
+   }
+  }
+
   return (
     <Formik
       initialValues={{ address: '', postalCode: '', city: '', country: '' }}
       validationSchema={Yup.object({
-        address: Yup.string().required('This field is required.').min('This address is too short.'),
-        postalCode: Yup.string().required('This field is required.').min('This postal code is too short.'),
-        city: Yup.string().required('This field is required.').min('This city is too short.'),
-        country: Yup.string().required('This field is required.').min('This country is too short.'),
+        address: Yup.string().required('This field is required.').min(1, 'This address is too short.'),
+        postalCode: Yup.string().required('This field is required.').min(1, 'This postal code is too short.'),
+        city: Yup.string().required('This field is required.').min(1, 'This city is too short.'),
+        country: Yup.string().required('This field is required.').min(1, 'This country is too short.'),
       })}
     >
       {(formik) => (
         <VStack as='form'>
-          <FormControl onChange={() => {}}>
+          <FormControl
+            onChange={
+              Object.keys(formik.errors).length === 0 && Object.keys(formik.touched).length >= 2
+                ? setErrorState(false, formik.values)
+                : setErrorState(true)
+            }
+          >
             <TextField name='address' placeholder='Street Address' label='Street Address' />
             <Flex>
               <Box flex='1' mr='10'>
