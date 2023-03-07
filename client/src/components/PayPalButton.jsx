7256739381d1a81ed519cc434a9a5285b44661cc
bodwood <bodiewood@hotmail.com>
2023-03-06 17:20:31 -0800

    add api from paypal to allow sandbox purchases with paypal

---
+++ client/src/components/PayPalButton.jsx
@@ -0,0 +1,34 @@
+// App.js
+import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
+import PAYPAL_CLIENT_ID from '../client_id';
+
+const PayPalButton = ({total, onPaymentSuccess, onPaymentError}) => {
+  return (
+    <PayPalScriptProvider options={{ 'client-id': PAYPAL_CLIENT_ID }}>
+      <PayPalButtons
+      forceReRender={[total()]}
+        createOrder={(data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                amount: {
+                  value: total(),
+                },
+              },
+            ],
+          });
+        }}
+        onApprove={(data, actions) => {
+          return actions.order.capture().then((details) => {
+           onPaymentSuccess(data)
+          });
+        }}
+        onError={(err) => {
+         onPaymentError(err)
+        }}
+      />
+    </PayPalScriptProvider>
+  );
+}
+
+export default PayPalButton;
