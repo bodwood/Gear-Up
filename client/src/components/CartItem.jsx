fd24f3fff9c806c6a726fd29b1d01c38e35f040f
bodwood <bodiewood@hotmail.com>
2023-03-07 15:52:57 -0800

    update cart component stack to include a link to the product page from the cart

--- client/src/components/CartItem.jsx
+++ client/src/components/CartItem.jsx
@@ -1,5 +1,7 @@
 import { CloseButton, Flex, Select, useColorModeValue as mode, Stack, Image, Box, Text } from '@chakra-ui/react';
+import { Link as ReactLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+
 import { addCartItem, removeCartItems } from '../redux/actions/cartActions';
 
 const CartItem = ({ cartItem }) => {
@@ -8,7 +10,7 @@ const CartItem = ({ cartItem }) => {
 
   return (
     <Flex direction={{ base: 'column', md: 'row' }} justify='space-between' align='center'>
-      <Stack direction='row' spacing='5' width='full'>
+      <Stack direction='row' spacing='5' width='full' as={ReactLink} to={`/product/${cartItem.id}`}>
         <Image rounded='lg' w='120px' h='120px' fit='cover' src={image} alt={name} draggable='false' loading='lazy' />
         <Box pt='4'>
           <Stack spacing='0.5'>
