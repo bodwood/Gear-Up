649b2486332e55a2f42efea939de999968249fd8
bodwood <bodiewood@hotmail.com>
2023-03-06 18:45:18 -0800

    add disabled function from passed in prop

--- client/src/components/PayPalButton.jsx
+++ client/src/components/PayPalButton.jsx
@@ -2,10 +2,11 @@
 import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
 import PAYPAL_CLIENT_ID from '../client_id';
 
-const PayPalButton = ({total, onPaymentSuccess, onPaymentError}) => {
+const PayPalButton = ({total, onPaymentSuccess, onPaymentError, disabled}) => {
   return (
     <PayPalScriptProvider options={{ 'client-id': PAYPAL_CLIENT_ID }}>
       <PayPalButtons
+      disabled={disabled}
       forceReRender={[total()]}
         createOrder={(data, actions) => {
           return actions.order.create({
