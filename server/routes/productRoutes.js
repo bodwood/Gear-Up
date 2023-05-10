7b81df4cfcd7e6881d732629d37fb29a854f989b
bodie wood <bodie1@macbook-air.lan>
2023-05-10 11:11:54 -0700

    Add request for adding product reviews

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import Product from '../models/Product.js';
+import asyncHandler from 'express-async-handler';
+import User from '../models/User.js';
+import protectRoute from '../middleware/authMiddleware.js';
 
 const productRoutes = express.Router();
 
@@ -19,7 +22,48 @@ const getProduct = async (req, res) => {
   }
 };
 
+const createProductReview = asyncHandler(async (req, res) => {
+
+  //From request body we are grabbing the data needed for finding the body and the user.
+  const { rating, comment, userId, title } = req.body;
+  const product = await Product.findById(req.params.id);
+  const user = await User.findById(userId);
+
+  //Checks if user has already reviewed the product.
+  //Users are only allowed to review a product one time.
+  //If a user tries to review more than once, a 400 error will be thrown.
+  if (product) {
+    const alreadyReviewed = product.reviews.find((rev) => rev.user.toString() === user._id.toString());
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error('Product already reviewed.');
+    }
+    //If the user has not already reviewed the product, a new review object is created.
+    const review = {
+      name: user.name,
+      rating: Number(rating),
+      comment,
+      title,
+      user: user._id,
+    };
+    //Product review is pushed to the reviews of the product and the number of reviews is updated.
+    product.reviews.push(review);
+    product.numberOfReviews = product.reviews.length();
+    //Product rating is updated to include all product reviews, to display the average rating.
+    product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length;
+
+    //Save or throw error.
+    await product.save();
+    res.status(201).json({ message: 'Review has been saved.' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found.');
+  }
+});
+
 productRoutes.route('/').get(getProducts);
 productRoutes.route('/:id').get(getProduct);
+productRoutes.route('/reviews/:id').post(protectRoute, createProductReview);
 
 export default productRoutes;
