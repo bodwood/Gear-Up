f467473f7f207bc890b8c9aff766c4ac60a631de
bodwood <bodiewood@hotmail.com>
2023-05-19 15:47:51 -0700

    Add component that will allow admin users to remove other users

---
+++ client/src/components/ConfirmRemovalAlert.jsx
@@ -0,0 +1,16 @@
+import {
+  Button,
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogBody,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+} from '@chakra-ui/react';
+import { useDispatch } from 'react-redux';
+
+const ConfirmRemovalAlert = () => {
+  return <div></div>;
+};
+
+export default ConfirmRemovalAlert;
--- client/src/components/UsersTab.jsx
+++ client/src/components/UsersTab.jsx
@@ -1,7 +1,110 @@
-import React from 'react'
+import {
+  Box,
+  TableContainer,
+  Th,
+  Tr,
+  Table,
+  Td,
+  Thead,
+  Tbody,
+  Button,
+  useDisclosure,
+  Alert,
+  Stack,
+  Spinner,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  Wrap,
+  useToast,
+} from '@chakra-ui/react';
+import { CheckCircleIcon, DeleteIcon } from '@chakra-ui/icons';
+import { useEffect, useState, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllUsers, deleteUser, resetErrorAndRemoval } from '../redux/actions/adminActions';
 
 const UsersTab = () => {
- return <div>UsersTab</div>
-}
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef();
+  const [userToDelete, setUserToDelete] = useState('');
+  const dispatch = useDispatch();
+  const admin = useSelector((state) => state.admin);
+  const user = useSelector((state) => state.user);
+  const { error, loading, userRemoval, userList } = admin;
+  const { userInfo } = user;
+  const toast = useToast();
+
+  useEffect(() => {
+    dispatch(getallUsers());
+    dispatch(resetErrorAndRemoval());
+    if (userRemoval) {
+      toast({ description: 'User has been removed.', status: 'success', isClosable: true });
+    }
+  }, [userRemoval]);
+
+  const openDeleteConfirmBox = (user) => {
+    setUserToDelete(user);
+    onOpen();
+  };
+  return (
+    <Box>
+      {error && (
+        <Alert status='error'>
+          <AlertIcon />
+          <AlertTitle>Uh Oh!</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      )}
+      {loading ? (
+        <Wrap justify='center'>
+          <Stack direction='row' spacing='4'>
+            <Spinner mt='20' thickness='2px' speed='0.65s' emptyColor='grey.200' color='orange.500' size='xl' />
+          </Stack>
+        </Wrap>
+      ) : (
+        <Box>
+          <TableContainer>
+            <Table variant='simple'>
+              <Thead>
+                <Tr>
+                  <Th>Name</Th>
+                  <Th>Email</Th>
+                  <Th>Registered</Th>
+                  <Th>Admin</Th>
+                  <Th>Action</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {userList &&
+                  userList.map((user) => (
+                    <Tr key={user.id}>
+                      {/* Shows logged in user their info */}
+                      <Td>
+                        {user.name} {user._id === userInfo._id ? '(You)' : ''}
+                      </Td>
+                      <Td>{user.email}</Td>
+                      <Td>{new Date(user.createdAt).toDateString()}</Td>
+                      <Td>{user.isAdmin === 'true' ? <CheckCircleIcon color='orange.500' /> : ''}</Td>
+                      <Td>
+                       {/* Button allows user to delete other users */}
+                        <Button
+                          disabled={user._id === userInfo.id}
+                          variant='outline'
+                          onClick={() => openDeleteConfirmBox(user)}
+                        >
+                          <DeleteIcon mr='5px' />
+                          Remove User
+                        </Button>
+                      </Td>
+                    </Tr>
+                  ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
+      )}
+    </Box>
+  );
+};
 
 export default UsersTab;
